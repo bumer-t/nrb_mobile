b909affa846af8ceb630d2979d81eb39ee208b28
bumer-t <bumer-t@yandex.ru>
2014-03-17 13:49:34 +0200

    www/deposit_created_finish.html && hash_url

--- www/static/js/mobile.js
+++ www/static/js/mobile.js
@@ -16,6 +16,16 @@ function getPhoneCode() {
     }
 }
 
+function change_hash_url(hash_url) {
+    location.hash = hash_url;
+}
+
+//
+function include_html(celector, template_name) {
+    celector.load(template_name+'.html', function(){$(this).trigger("create")});
+    change_hash_url(template_name);
+}
+
 function send_phone() {
     if ($('#phone_number').val().length < 7) {
         alert('Не введен номер');
@@ -26,6 +36,7 @@ function send_phone() {
         return false;
     }
     //success
+    change_hash_url('auth_otp');
     $('#login_phone').hide();
     $('#login_code').show();
 }
@@ -36,9 +47,8 @@ function send_otp() {
         return false;
     }
     //success
-    alert('new page soon');
-//    $('#div_my_deposits').load('my_deposits.html').trigger("create");
-    $("#div_my_deposits").load('my_deposits.html', function(){$(this).trigger("create")});
+//    $("#div_my_deposits").load('my_deposits.html', function(){$(this).trigger("create")});
+    include_html($("#div_my_deposits"),'my_deposits');
     $('#div_login_forms').hide();
     $('#div_my_deposits').show();
 }
@@ -67,26 +77,38 @@ $(function(){
         }
     });
 
-    //Показывает форму оформления депозита
-    $(document).on('touchstart click', '.create_deposit', function(e){
-        e.stopPropagation(); e.preventDefault();
-        console.log('create');
-        var temp_currency = $('#head_currency .ui-state-active').contents().attr('href');
-        var currency = temp_currency.substring(1,temp_currency.length);
-        $('#'+currency).find('table').hide();
-        $('#'+currency).find('.div_create_deposit').load('deposit_create.html', function(){$(this).trigger("create")});
-        $('#'+currency).find('.div_create_deposit').show();
-    });
-
     //клик по Депозиту
     $('.deposit').on('touchstart click', function(e){
         e.stopPropagation(); e.preventDefault();
         var id_deposit = ($(this).attr('id'));
         var title = $('#' + id_deposit).find('.dep_name').text();
         $('#title').text(title);
-        $("#div_deposit_info").load('deposit_info.html', function(){$(this).trigger("create")});
+        include_html($("#div_deposit_info"),'deposit_info');
+//        $("#div_deposit_info").load('deposit_info.html', function(){$(this).trigger("create")});
         $('#deposits_list').hide();
         $('#div_deposit_info').show();
+    });
+
+    //Показывает форму оформления депозита
+    $(document).on('touchstart click', '.place_deposit', function(e){
+        e.stopPropagation(); e.preventDefault();
+        console.log('оформление');
+        var temp_currency = $('#head_currency .ui-state-active').contents().attr('href');
+        var currency = temp_currency.substring(1,temp_currency.length);
+        $('#'+currency).find('table').hide();
+        include_html($('#'+currency).find('.div_create_deposit'),'deposit_create');
+//        $('#'+currency).find('.div_create_deposit').load('deposit_create.html', function(){$(this).trigger("create")});
+        $('#'+currency).find('.div_create_deposit').show();
+    });
+
+    //здесь будет запрос на оформление депозита
+    $(document).on('touchstart click', '#deposit_create', function(e){
+        e.stopPropagation(); e.preventDefault();
+        console.log('create');
+//        $("#result").load('deposit_created_finish.html', function(){$(this).trigger("create")});
+        include_html($("#result"),'deposit_created_finish');
+        $('#div_main').hide();
+        $('#result').show();
     });
 
     //set
