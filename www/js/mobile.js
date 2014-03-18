ee0d31033962769e8db2e49ec2360537e29a6b40
bumer-t <bumer-t@yandex.ru>
2014-03-18 14:47:36 +0200

    new test

--- www/js/mobile.js
+++ www/js/mobile.js
@@ -16,17 +16,6 @@ function getPhoneCode() {
     }
 }
 
-function change_hash_url(hash_url) {
-    location.hash = hash_url;
-}
-
-//
-function include_html(celector, template_name) {
-    celector.load(template_name+'.html', function(){$(this).trigger("create")});
-    change_hash_url(template_name);
-    document.addEventListener("backbutton", onBackKeyDown, false);
-}
-
 function send_phone() {
     if ($('#phone_number').val().length < 7) {
         alert('Не введен номер');
@@ -37,7 +26,6 @@ function send_phone() {
         return false;
     }
     //success
-    change_hash_url('auth_otp');
     $('#login_phone').hide();
     $('#login_code').show();
 }
@@ -48,29 +36,39 @@ function send_otp() {
         return false;
     }
     //success
-//    $("#div_my_deposits").load('my_deposits.html', function(){$(this).trigger("create")});
-    include_html($("#div_my_deposits"),'my_deposits');
+    alert('new page soon');
+//    $('#div_my_deposits').load('my_deposits.html').trigger("create");
+    $("#div_my_deposits").load('my_deposits.html', function(){$(this).trigger("create")});
     $('#div_login_forms').hide();
     $('#div_my_deposits').show();
 }
 
-function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, true);
-    alert('onLoad - deviceready');
+var createCookie = function(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-// Cordova is loaded and it is now safe to call Cordova methods
-//
-function onDeviceReady() {
-    // Register the event listener
-    document.addEventListener("backbutton", onBackKeyDown, true);
-}
-
-// Handle the back button
-//
-function onBackKeyDown() {
-    alert(1111);
-    alert(location.hash);
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
 }
 
 $(function(){
@@ -103,8 +101,7 @@ $(function(){
         var id_deposit = ($(this).attr('id'));
         var title = $('#' + id_deposit).find('.dep_name').text();
         $('#title').text(title);
-        include_html($("#div_deposit_info"),'deposit_info');
-//        $("#div_deposit_info").load('deposit_info.html', function(){$(this).trigger("create")});
+        $("#div_deposit_info").load('deposit_info.html', function(){$(this).trigger("create")});
         $('#deposits_list').hide();
         $('#div_deposit_info').show();
     });
@@ -116,8 +113,7 @@ $(function(){
         var temp_currency = $('#head_currency .ui-state-active').contents().attr('href');
         var currency = temp_currency.substring(1,temp_currency.length);
         $('#'+currency).find('table').hide();
-        include_html($('#'+currency).find('.div_create_deposit'),'deposit_create');
-//        $('#'+currency).find('.div_create_deposit').load('deposit_create.html', function(){$(this).trigger("create")});
+        $('#'+currency).find('.div_create_deposit').load('deposit_create.html', function(){$(this).trigger("create")});
         $('#'+currency).find('.div_create_deposit').show();
     });
 
@@ -125,23 +121,17 @@ $(function(){
     $(document).on('touchstart click', '#deposit_create', function(e){
         e.stopPropagation(); e.preventDefault();
         console.log('create');
-//        $("#result").load('deposit_created_finish.html', function(){$(this).trigger("create")});
-        include_html($("#result"),'deposit_created_finish');
+        $("#result").load('deposit_created_finish.html', function(){$(this).trigger("create")});
         $('#div_main').hide();
         $('#result').show();
     });
+    
+    //
+    createCookie('qwe1', '1111',2);
+    alert(getCookie('qwe1'));
 
     //set
 //    window.localStorage.setItem("key", "value");
     //get
 //    var value = window.localStorage.getItem("key");
-    change_hash_url('homepage');
-
-    document.addEventListener("backbutton", onBackKeyDown1, true);
-
-    function onBackKeyDown1() {
-        alert(222222);
-        alert(location.hash);
-    }
-
 });
